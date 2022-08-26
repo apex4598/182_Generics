fd78a3231a3a3b8456c40cfb4bf7ba084eab99b3
Apex4598 <login2ajit@gmail.com>
2022-08-26 07:16:22 +0530

    Welcome to Generics Program

---
+++ src/com/bridgelabz/Generics.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Generics<T extends Comparable<T>> {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generics program ");
+    }
+}
